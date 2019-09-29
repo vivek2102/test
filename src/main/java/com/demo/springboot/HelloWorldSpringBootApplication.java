cb806beb7fb0001956469f30b31d78d0107d0b9c
vivek2102 <vivnsec06@gmail.com>
2019-09-29 22:19:35 +0800

    Update HelloWorldSpringBootApplication.java

--- src/main/java/com/demo/springboot/HelloWorldSpringBootApplication.java
+++ src/main/java/com/demo/springboot/HelloWorldSpringBootApplication.java
@@ -8,5 +8,6 @@ public class HelloWorldSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldSpringBootApplication.class, args);
+		fsdfsdfsfdsf
 	}
 }
