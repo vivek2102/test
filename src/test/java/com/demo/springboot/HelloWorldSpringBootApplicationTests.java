186262abc75598c8c962ec8bdb6c9923feb9da6f
vivek2102 <vivnsec06@gmail.com>
2019-10-05 20:20:46 +0800

    Update HelloWorldSpringBootApplicationTests.java

--- src/test/java/com/demo/springboot/HelloWorldSpringBootApplicationTests.java
+++ src/test/java/com/demo/springboot/HelloWorldSpringBootApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class HelloWorldSpringBootApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void luckerNodes() {
 	}
 
 }
