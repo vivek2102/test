59ab34a73f9d6f90f495df7623a4888a4d6a75d2
vivek2102 <vivnsec06@gmail.com>
2020-02-13 16:55:45 +0800

    Commit

--- src/main/java/com/demo/springboot/HelloWorld.java
+++ src/main/java/com/demo/springboot/HelloWorld.java
@@ -1,18 +1,2 @@
-package com.demo.springboot;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloWorld {
-	@RequestMapping(value="/")
-	public String helloWorld() {
-		
-		return "Hello World1  ";
-		I am Testing Revert
-		Revert2
-	   
-	}
-
-}
+Revert1
 
--- src/main/java/com/demo/springboot/HelloWorldSpringBootApplication.java
+++ src/main/java/com/demo/springboot/HelloWorldSpringBootApplication.java
@@ -1,15 +1,1 @@
-package com.demo.springboot;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class HelloWorldSpringBootApplication {
-
-	public static void main(String[] args) {
-		"Learn Revert"
-		I am in revert
-		Revert2
-
-	}
-}
+Revert1
